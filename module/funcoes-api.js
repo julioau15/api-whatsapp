8d522d27ce558bf41500d9f23504cdfa4c3de3cd
julioau15 <julioaugustov@gmail.com>
2026-04-08 08:27:03 -0300

    first commit

---
+++ module/funcoes-api.js
@@ -0,0 +1,16 @@
+/***********************************************************************
+ * Objetivo: API WhatsApp
+ * Data: 08/04/2026
+ * Autor: Julio
+ * Versão: 1.0
+ * ********************************************************************/
+
+/**
+ * END-POINTS:
+ *  () Listar todos os dados de usuário independente do número
+ *  () Listar dados da conta do profile do usuário filtrando pelo número
+ *  () Listar dados de contato para cada usuário filtrando pelo número
+ *  () Listar todas as mensagens trocadas de uma conta de usuário filtrando pelo número
+ *  () Listar as mensagens trocadas de uma conta com um contato
+ *  () Filtro de mensagens por palavras-chave
+ */
